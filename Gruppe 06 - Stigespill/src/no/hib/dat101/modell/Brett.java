cb9be5e884959fd1d6b1781eb4af31985370ed66
h181192 <h181192@stud.hib.no>
2017-03-27 23:03:53 +0200

    V.0.1.1

--- Gruppe 06 - Stigespill/src/no/hib/dat101/modell/Brett.java
+++ Gruppe 06 - Stigespill/src/no/hib/dat101/modell/Brett.java
@@ -2,6 +2,17 @@ package no.hib.dat101.modell;
 
 import no.hib.dat101.modell.rute.Rute;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "brett", schema = "kristoffer_stigespill")
 public class Brett {
 	private final Integer ANTALL_RUTER = 100;
 	private Rute[] ruteTab;
