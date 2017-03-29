39a1f33125ca4a6c859e6a6b5c76a169de517c6a
h181192 <h181192@stud.hib.no>
2017-03-29 21:17:06 +0200

    Opprettet tekstgrensesnitt for stigespill

--- Gruppe 06 - Stigespill/src/no/hib/dat101/utsyn/Tekstgrensesnitt.java
+++ Gruppe 06 - Stigespill/src/no/hib/dat101/utsyn/Tekstgrensesnitt.java
@@ -1,5 +1,27 @@
 package no.hib.dat101.utsyn;
 
-public class Tekstgrensesnitt {
+import java.util.Scanner;
 
+import no.hib.dat101.modell.brikke.BrikkeFarge;
+
+public class Tekstgrensesnitt implements StigespillUI {
+	private Scanner tast;
+
+	@Override
+	public Integer lesAntallSpillere() {
+		Integer antall = tast.nextInt();
+		return antall;
+	}
+
+	@Override
+	public String lesInnSpiller() {
+		String navn = tast.nextLine();
+		return navn;
+	}
+
+	@Override
+	public BrikkeFarge lesInnBrikkeFarge() {
+		Integer nr = tast.nextInt();
+		return BrikkeFarge.finnBrikkeFarge(nr);
+	}
 }
