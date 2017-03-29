36212da3afb79cc40e3cfecdd199c1d6ffe5f5da
h181192 <h181192@stud.hib.no>
2017-03-29 22:05:50 +0200

    Små endringer

--- Gruppe 06 - Stigespill/src/no/hib/dat101/utsyn/Tekstgrensesnitt.java
+++ Gruppe 06 - Stigespill/src/no/hib/dat101/utsyn/Tekstgrensesnitt.java
@@ -6,22 +6,24 @@ import no.hib.dat101.modell.Spiller;
 import no.hib.dat101.modell.brikke.BrikkeFarge;
 
 public class Tekstgrensesnitt implements StigespillUI {
-	private Scanner tast;
+	private Scanner tast = new Scanner(System.in);
 
 	@Override
 	public Integer lesAntallSpillere() {
-		Integer antall = tast.nextInt();
-		return antall;
+		System.out.print("Oppi antall spillere: ");
+		return tast.nextInt();
 	}
 
 	@Override
 	public String lesInnSpiller() {
-		String navn = tast.nextLine();
-		return navn;
+		System.out.print("Oppgi navn på spiller: ");
+		return tast.next();
 	}
 
 	@Override
 	public BrikkeFarge lesInnBrikkeFarge() {
+		System.out.println("1 = RED, 2 = BLUE, 3 = GREEN, 4 = YELLOW");
+		System.out.print("Oppgi fargekode: ");
 		Integer nr = tast.nextInt();
 		return BrikkeFarge.finnBrikkeFarge(nr);
 	}
