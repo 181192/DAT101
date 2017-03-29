6de5e782a9e741f7a4bb0130ee8bfd34d54a6a18
h181192 <h181192@stud.hib.no>
2017-03-29 22:05:25 +0200

    Opprettet main for å kjøre spillet

--- Gruppe 06 - Stigespill/src/no/hib/dat101/klient/StartSpill.java
+++ Gruppe 06 - Stigespill/src/no/hib/dat101/klient/StartSpill.java
@@ -1,10 +1,23 @@
 package no.hib.dat101.klient;
 
+import java.util.Scanner;
+
 import no.hib.dat101.modell.Stigespill;
+import no.hib.dat101.utsyn.Tekstgrensesnitt;
 
 public class StartSpill {
 	public static void main(String[] args) {
-		Stigespill stiges = new Stigespill();
+//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("eclipselink");
+//		EntityManager em = entityManagerFactory.createEntityManager();
+//
+//		em.close();
+//		entityManagerFactory.close();
+
+		Scanner tast = new Scanner(System.in);
+		Stigespill stiges = new Stigespill(new Tekstgrensesnitt());
 		stiges.start();
+		tast.close();
+		
+
 	}
 }
