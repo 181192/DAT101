7d547240303d7236e079a801115c1874cb2f4b31
h181192 <h181192@stud.hib.no>
2017-04-23 22:11:49 +0200

    main

--- Gruppe 06 - Utleie/src/no/hib/dat101/klient/KlientReservasjon.java
+++ Gruppe 06 - Utleie/src/no/hib/dat101/klient/KlientReservasjon.java
@@ -6,12 +6,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import no.hib.dat101.modell.Reservasjon;
+
 public class KlientReservasjon {
 	public static void main(String[] args) {
 		Scanner tast = new Scanner(System.in);
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("eclipselink");
 		EntityManager em = entityManagerFactory.createEntityManager();
-
+		Reservasjon r = new Reservasjon();
+		r.lagReservasjon();
+		
 		em.close();
 		entityManagerFactory.close();
 		tast.close();
